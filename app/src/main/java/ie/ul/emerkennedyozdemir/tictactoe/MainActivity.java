2e8752e07899b030719265def7737ca261441dd0
EmerKO <43354325+EmerKO@users.noreply.github.com>
2018-10-08 23:01:34 +0100

    finshed the view

--- app/src/main/java/ie/ul/emerkennedyozdemir/tictactoe/MainActivity.java
+++ app/src/main/java/ie/ul/emerkennedyozdemir/tictactoe/MainActivity.java
@@ -1,7 +1,8 @@
 package ie.ul.emerkennedyozdemir.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,4 +15,19 @@ public class MainActivity extends AppCompatActivity {
 
         mGame = new TicTacToeGame( this);
     }
+
+    public void pressedSquare(View view) {
+       String tagAsStr = view.getTag().toString();
+       int tagAsInt = Integer.parseInt(tagAsStr);
+       //Log.d( "TTT", "You pressed index " + tagAsInt);
+
+       // Toast.makeText( this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
+
+    }
+    public void pressedNewGame(View view) {
+
+
+
+    }
+
 }
